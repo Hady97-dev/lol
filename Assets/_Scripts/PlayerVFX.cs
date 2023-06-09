d224e1e49fa3dfa0743cb0d3f92ea1e06bc05194
Hady97-dev <ahmedhandasa97@outlook.com>
2023-06-09 15:22:54 +0200

    editing player VFX

--- Assets/_Scripts/PlayerVFX.cs
+++ Assets/_Scripts/PlayerVFX.cs
@@ -4,35 +4,36 @@ using UnityEngine;
 
 public class PlayerVFX : MonoBehaviour
 {
-    private Transform player;
+    //private Transform player;
     public GameObject Ability1_Mesh;
     public float Ability1_Speed;
-    public GameObject target;
+    public Transform target;
     public Transform FirePoint;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        
+        //player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        target = GameObject.FindGameObjectWithTag("target").GetComponent<Transform>();
+        FirePoint = GameObject.FindGameObjectWithTag("LeftHand").GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-       
+        
+
     }
 
     public void PlayerAbility1_VFX()
     {
-       var instance =  (Instantiate(Ability1_Mesh, player.position, Quaternion.identity));  // we used var insance   as you can not add effects to a prefab 
-        instance.GetComponent<Rigidbody>().AddForce((target.transform.position - player.transform.position).normalized
+       var instance =  (Instantiate(Ability1_Mesh, FirePoint.position, Quaternion.identity));  // we used var insance   as you can not add effects to a prefab 
+        instance.GetComponent<Rigidbody>().AddForce((target.position - FirePoint.position).normalized
             * Ability1_Speed, ForceMode.Impulse);      // very important line 
-        Destroy (instance,3f);
-
-        print("pppppp");
+        Destroy (instance,3f);                
 
     }
 }
